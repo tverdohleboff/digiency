ae3497aeaa310af7512adc07a6d6d8130a5ef3d0
Alex <tverdohleboff@gmail.com>
2023-01-06 17:34:46 +0400

    page «our work»: add active tab and coming soon

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
 import '../scss/main.scss';
+
+const tabs = document.querySelectorAll('.our-work__examples-category');
+for (const tab of tabs) {
+  tab.addEventListener('click', clickTab);
+}
+
+function clickTab(event) {
+  const tab = event.target;
+  const dataTab = tab.getAttribute('data-tab');
+
+  const selectedTab = document.querySelector('.selected');
+  selectedTab.classList.remove('selected');
+  tab.classList.add('selected');
+
+  const activeTabsContent = document.querySelectorAll('.active');
+  for (const activeTabContent of activeTabsContent) {
+    activeTabContent.classList.remove('active');
+  }
+  const nextActiveTabsContent = document.querySelectorAll(
+    `[data-tab-content="${dataTab}"]`
+  );
+  for (const nextActiveTabContent of nextActiveTabsContent) {
+    nextActiveTabContent.classList.add('active');
+  }
+}
