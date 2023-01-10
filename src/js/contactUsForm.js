679abff80067df8d1c6373bdf391b2e79e7a9a0a
Alex <tverdohleboff@gmail.com>
2023-01-10 14:12:58 +0400

    Adaptive design «Contact us» 30%

--- src/js/contactUsForm.js
+++ src/js/contactUsForm.js
@@ -2,7 +2,7 @@ function initializeContactUsForm() {
   const form = document.querySelector('#contactUsForm');
   form.addEventListener('submit', function (event) {
     event.preventDefault();
-    alert('KGB has received your message!');
+    alert('We received your message.');
     const inputs = document.querySelectorAll(
       '.contact-us__message-send-block-input'
     );
