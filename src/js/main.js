8d5a219a1ec526438d2ecdffaf1dba05daf59ca9
Alex <tverdohleboff@gmail.com>
2023-01-12 22:37:09 +0400

    Add header

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,12 @@ import '../scss/main.scss';
 import initializeTabs from './tabs';
 import initializeContactUsForm from './contactUsForm';
 import initializeSubscribeForm from './subscribeForm';
+import initializeScrollToTop from './scrollToTop';
 
 initializeContactUsForm();
 initializeTabs();
 initializeSubscribeForm();
+initializeScrollToTop();
 
 // import initializeCarousel from './carousel';
 // initializeCarousel();
---
+++ src/js/scrollToTop.js
@@ -0,0 +1,14 @@
+function initializeScrollToTop() {
+  const button = document.querySelector('.copyright__main-button');
+  button.addEventListener('click', scrollToTop);
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
+}
+
+export default initializeScrollToTop();
