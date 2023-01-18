8fa7b7cec3f9ab45e08862801b46bf6d8408fd66
Alex <tverdohleboff@gmail.com>
2023-01-18 20:44:29 +0400

    Add autoprefixer

--- production.config.js
+++ production.config.js
@@ -1,7 +1,14 @@
+import autoprefixer from 'autoprefixer';
+
 export default {
   base: 'https://tverdohleboff.github.io/digiency/',
   mode: 'production',
   build: {
     outDir: 'docs',
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
   },
 };
